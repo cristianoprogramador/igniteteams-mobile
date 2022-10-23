b2539813f6a4f3d1c55eab1ff916e78453990803
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-23 17:20:33 -0300

    Projeto inicializado e a base de estilo criada

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+
+import { Header } from '@components/Header';
+import { Container } from './styles';
+
+export function Groups() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
